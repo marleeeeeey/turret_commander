2f47776d737abfde140e6ed4e9905db47b74e53f
marleeeeeey <marleeeeeey@gmail.com>
2024-05-15 22:51:33 +0400

    Add zoom_in, zoom_out actions from controller.

--- common/scene_spaceship/camera_2d.gd
+++ common/scene_spaceship/camera_2d.gd
@@ -2,13 +2,10 @@ extends Camera2D
 
 @export var zoom_speed = 1.1
 
-func _input(event: InputEvent) -> void:
-	var event_mouse_button = event as InputEventMouseButton
-	if not event_mouse_button:
-		return
 
-	if event_mouse_button.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-		zoom /= zoom_speed
-
-	if event_mouse_button.button_index == MOUSE_BUTTON_WHEEL_UP:
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("zoom_in"):
 		zoom *= zoom_speed
+
+	if Input.is_action_pressed("zoom_out"):
+		zoom /= zoom_speed
