7392ce749abb5af24e90c8f4f1287edfccbb7c7e
marleeeeeey <marleeeeeey@gmail.com>
2024-05-21 10:28:30 +0400

    Main scene is empty. Code behind only.

--- this/main.gd
+++ this/main.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 @export var game_logic_scene: PackedScene
+@export var pause_menu_scene: PackedScene
 
+# Create game_logic when game begin. Destroy at the end.
 var game_logic = null
+var pause_menu = null
 
 func _ready() -> void:
 	game_logic = game_logic_scene.instantiate()
 	add_child(game_logic)
+	pause_menu = pause_menu_scene.instantiate()
+	add_child(pause_menu)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = true
-		$PauseMenu.process_mode = Node.PROCESS_MODE_ALWAYS
+		# Pause menu disabled by default. Enalble pause menu processing here
+		# while other part of the game has stopped.
+		pause_menu.process_mode = Node.PROCESS_MODE_ALWAYS
